767a293a2480d57a58812d1318b4f864a66c8c5d
phlabsdevops <harinathbeeram1999@gmail.com>
2024-01-15 16:23:38 +0530

    Running roboshop-shell-script folder

--- roboshop.sh
+++ roboshop.sh
@@ -12,7 +12,8 @@ INSTANCE_TYPE="t3.small"
 else
 INSTANCE_TYPE="t2.micro"
 fi
-aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0c0883cad63eb869b
+aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0c0883cad63eb869b --tag-specifications "ResourceType=instance,
+Tags=[{Key=Name,Value=$i}]"
 
 done
 
